6d4ba90b5a1c58b62aa84f1c6ed3c4cc401569b3
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-23 21:55:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_22_Static_Attrribute_ConsoleApp
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songCount = 0;
+        public Songs(string aTitle, string aArtist, int aDuration)
+        {
+            this.title = aTitle;
+            this.artist = aArtist;
+            this.duration = aDuration;
+            songCount++;  //increment each time a song is added
+        }
+
+        static void Main(string[] args)
+        {
+            Songs holiday = new Songs("Holiday", "Green day", 2);
+            Songs kashmir = new Songs("Kashmir", "Led Zeppelin", 5);
+
+
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
